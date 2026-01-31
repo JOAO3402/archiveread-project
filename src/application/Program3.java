482c5016c38210d9ca912b6f20158e2ffea7293e
Joao Alexandre <jo.alexandre.ber@gmail.com>
2026-01-31 10:58:50 -0300

    Program4 criado

--- src/application/Program3.java
+++ src/application/Program3.java
@@ -15,7 +15,7 @@ public class Program3 {
 
         String path = "c:\\temp\\out.txt";
 
-        try(BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(path))){
             for(String line : lines){
                 bw.write(line);
                 bw.newLine();
